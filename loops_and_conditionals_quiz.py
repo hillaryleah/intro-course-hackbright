8919082af81b6ea12737e962a5fadb67d22d3086
Hillary Leah <hillary@Hillarys-MacBook-Pro.local>
2016-02-16 23:29:11 -0800

    Adding loops and conditionals quiz.

--- loops_and_conditionals_quiz.py
+++ loops_and_conditionals_quiz.py
@@ -1,22 +1,34 @@
-#1 Write a function that takes in a number and prints that number of rows and columns of *
+# Mid- Session Take Home Quiz: Loops and Conditionals
 
+# 1. Write a function that takes in a number and prints that number of rows and columns of *
+
+# create function with single parameter
 def draw_asterisks(num):
+	# for every item within the num range
 	for i in range(num): 
+		# print asterisk(s) on row. num of asterisk(s) = num (argument passed when function called)
 		print "*" * num
-draw_asterisks(1)	
+
+# call the function, pass the num 8. Function will be intialized 8 times, which 
+  # results in 8 rows of 8 asterisks being printed		
+draw_asterisks(8)	
+print 
+
 
 # 2 Write a function that draws a checkerboard with x's and o's 
-# A checkerboard is an 8 x 8 alternating grid
 
+# create function that takes no parameters
 def draw_checkboard():
-	#setting the range as 8 since there are 8 rows
+	# setting the range as 8 since there are 8 rows for the checkboard
+	  # so function needs to fun 8 times 
 	for i in range(8): 
-		# conditional to print 1 variation of the line on even rows
+		# print variation1 on even numbered rows
 		if i % 2 == 0: 
 			print "x o x o x o x o"
-		#print the other variation on odd rows
+		# print variation2 on off numbered rows
 		else: 
 			print "o x o x o x o x"	
 
+# call the function 
 draw_checkboard()
 
